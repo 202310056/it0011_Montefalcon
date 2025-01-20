7318f1c1e08551204f9b6844f7e71c5ebc1b4563
202310056 <jennimontefalcon@gmail.com>
2025-01-20 10:50:15 +0800

    First Activity

--- TW23/act1.py
+++ TW23/act1.py
@@ -1,2 +1,14 @@
-name = "Jennifer"
-print("My name is: ", name)
+fname = input("Enter First Name: ")
+lname = input("Enter Last Name: ")
+mname = input("Enter Middle Name: ")
+bday = input("Enter Birthdate: ")
+gender = input("What is your gender? ")
+address = input("What is your address? ")
+course = input("What is your course? ")
+yrlvl = input("What is your year level? ")
+
+print("Hi there! My name is ", fname, lname,"and my middle name is ", mname)
+print("My birthday is on",bday)
+print("I am a",gender)
+print("I live at ",address)
+print("I am currently taking ",course, "and I am now a", yrlvl,". :)")
